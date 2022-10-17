63aa9d4df830c6b89d034e748c234692f40b70a5
JanetTirador <113356729+Janettirador@users.noreply.github.com>
2022-10-17 02:57:05 +0000

    still trying the file. wanted to see error code

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
-class main {
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Names{
   public static void main(String[] args) {
-
-    int names = myScannerinput.nextInt();
-    Scanner myScanner =null;
-
+    ArrayList<Names> arrayList = new ArrayList<>();
+      
     System.out.println("Press 1 to learn about salary.");
     System.out.println("Press 2 to learn about the job.");
     System.out.println("Press 3 to learn about demand.");
@@ -18,7 +21,6 @@ class main {
         if(choiceEntry < 1 || choiceEntry > 5) {
         choiceentry = scanchoice.nextInt(); 
         }
-
         else if(choiceEntry == 1) {
         System.out.println("$98,345 average salary in south Florida!");
         }
@@ -28,8 +30,18 @@ class main {
         else if(choiceEntry == 3) {
         System.out.println("Top 10 Forbes In-Demand Jobs!");
         }
-        else(choiceEntry == 4) {
-        myScanner = new Scanner(new File("names.txt"));
+        else(choiceEntry == 4) {//name file goes heree
+            try{
+            FileReader fileReader = new FileReader("names.txt");
+            Scanner scanner = new Scanner(fileReader);
+            while(scanner.hasNextLine()){
+            String [] curline = scanner.nextline().split(",");
+            for(String s : curline){
+            System.out.println(s);
+            }
+            catch(FileNotFoundException f)
+            {
+        }  System.out.println("testinggg");
       }
     }
   }
