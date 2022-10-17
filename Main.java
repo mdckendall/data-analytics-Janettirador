30c03e0902ac84b0e2e71d3ff71f8459a874fbdb
JanetTirador <113356729+Janettirador@users.noreply.github.com>
2022-10-17 00:42:56 +0000

    added menu

--- Main.java
+++ Main.java
@@ -1,14 +1,31 @@
-import java.io.FileReader;
-import java.util.Scanner;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
+class main {
+  public static void main(String[] args) {
 
-class Main {
-	public static void main(String[] args) throws FileNotFoundException {
+    System.out.println("Press 1 to learn about salary.");
+    System.out.println("Press 2 to learn about the job.");
+    System.out.println("Press 3 to learn about demand.");
+    System.out.println("Press 4 to view current students.");
+    System.out.println("Press 5 to quit.");
 
-		ArrayList<String> arrayList = new ArrayList<>();
-		FileReader fr = new FileReader("names.txt");
-		Scanner fileScanner = new Scanner(fr);
-		
-	}
+        int choiceEntry;
+    do {
+      System.out.println("");
+        switch (choiceEntry)
+    {
+          case 1:
+            System.out.println("$98,345 average salary in south Florida!");
+            break;
+          case 2: 
+             System.out.println("US News - 100 Best Jobs!");
+            break;
+          case 3: 
+            System.out.println("Top 10 Forbes In-Demand Jobs!");
+            break;
+          case 4:
+            System.out.println("Top 10 Forbes In-Demand Jobs!");
+          default:
+            System.out.println("");
+      }   
+    }   while (choiceEntry != 5);
+  }
 }
